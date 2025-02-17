4e2996b4a6e649ff008bc77dce2d3cd0907e1be0
Maia Munich <maia.munich@gmail.com>
2025-02-16 19:31:32 -0500

    array trial

---
+++ assignment/homework-4/homework-4-1.js
@@ -0,0 +1,66 @@
+const cats = [
+    {
+        name: 'Charlie',
+        adoptionStatus: 'available'
+    },
+    {
+        name: 'Lily',
+        adoptionStatus: 'not-available'
+    },
+    {
+        name: 'Coco',
+        adoptionStatus: 'available'
+    },
+    {
+        name: 'Oreo',
+        adoptionStatus: 'not-available'
+    },
+    {
+        name: 'Luna',
+        adoptionStatus: 'available'
+    },
+    {
+        name: 'Milo',
+        adoptionStatus: 'available'
+    },
+    {
+        name: 'Lola',
+        adoptionStatus: 'not-available'
+    },
+    {
+        name: 'Leo',
+        adoptionStatus: 'available'
+    },
+    {
+        name: 'Willow',
+        adoptionStatus: 'available'
+    },
+    {
+        name: 'Bella',
+        adoptionStatus: 'not-available'
+    },
+    {
+        name: 'Max',
+        adoptionStatus: 'available'
+    },
+    {
+        name: 'Cleo',
+        adoptionStatus: 'available'
+    },
+    {
+        name: 'Lucy',
+        adoptionStatus: 'not-available'
+    },
+    {
+        name: 'Daisy',
+        adoptionStatus: 'available'
+    },
+];
+
+const cat = {name:"Pinecone", age:13, type:'Munchkin', cuteness: 9001};
+const cat2 ={adoptionStatus: 'available'}
+console.log(cat.name);
+console.log(cat2.adoptionStatus);
+
+const availableCats = cats.filter(cat => cat.adoptionStatus === 'available');
+console.log(availableCats);
