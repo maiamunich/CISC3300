462ac161da9abcd063900657903dfbf0813af434
Maia Munich <maia.munich@gmail.com>
2025-02-20 17:17:26 -0500

    changes

--- in-class/in-class-11/getData.php
+++ in-class/in-class-11/getData.php
@@ -1,16 +1,40 @@
 <?php
-// Set header to return JSON
-header('Content-Type: application/json');
 
-// Create an associative array
-$data = array(
-    "name" => "Charlie",
-    "type" => "Cat",
-    "age" => 3,
-    "color" => "Orange",
-    "isAdopted" => true
-);
+// Set headers for JSON and CORS
+header("Content-Type: application/json");
+header("Access-Control-Allow-Origin: *");
 
-// Return JSON encoded array
-echo json_encode($data);
+$uri = strtok($_SERVER["REQUEST_URI"], '?');
+
+//get uri pieces
+$uriArray = explode("/", $uri);
+//var_dump($uriArray);
+
+// Your cats data
+$cats = [
+    [
+        'name' => 'Charlie',
+        'adoptionStatus' => 'available'
+    ],
+    [
+        'name' => 'Lily',
+        'adoptionStatus' => 'not-available'
+    ],
+    // ... you can add more cats from your previous array
+];
+
+// Check if the request is for cats data
+if ($uriArray[1] === 'cats' && $_SERVER['REQUEST_METHOD'] === 'GET') {
+    echo json_encode($cats);
+    exit();
+}
+
+if ($uriArray[1] === 'form' && $_SERVER['REQUEST_METHOD'] === 'POST') {
+    echo json_encode([
+        'message' => 'Success'
+    ]);
+    exit();
+}
+
+
 ?>
