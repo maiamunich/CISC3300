a21956913675faac8e3f7b101620cebe89a89487
Maia Munich <maia.munich@gmail.com>
2025-02-24 16:52:45 -0500

    COmmit

--- in-class/in-class-12/index-2.php
+++ in-class/in-class-12/index-2.php
@@ -1,9 +1,39 @@
 <?php
+$request = $_SERVER['REQUEST_URI'];
+$path = parse_url($request, PHP_URL_PATH);
 
-echo '
-    <div>
-      <b>Hello, This is the code for this class</b>
-    </div>
-    '
+switch ($path) {
+    case '/html':
+        header('Content-Type: text/html');
+        echo '
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <title>HTML Response</title>
+        </head>
+        <body>
+            <h1>HTML is working</h1>
+            <p>Hi Maia here!</p>
+        </body>
+        </html>';
+        break;
 
+    case '/json':
+        header('Content-Type: application/json');
+        $data = array(
+            'message' => 'JSON is working',
+            'items' => array(
+                'it1' => 'v1',
+                'it2' => 'v2',
+                'it3' => 'v3'
+            ),
+            'success' => true
+        );
+        echo json_encode($data);
+        break;
+
+    default:
+        echo 'Try accessing /html or /json paths';
+        break;
+}
 ?>
