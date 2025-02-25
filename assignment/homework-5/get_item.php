87f6fe91e9f093da1570deb4fc6b29c876c76347
Maia Munich <maia.munich@gmail.com>
2025-02-25 17:13:26 -0500

    commiting homework 5

---
+++ assignment/homework-5/get_item.php
@@ -0,0 +1,25 @@
+<?php
+header('Content-Type: application/json');
+
+// Sample business data - you can modify this
+$items = array(
+    array(
+        "name" => "Artisan Coffee",
+        "price" => 4.99
+    ),
+    array(
+        "name" => "Fresh Croissant",
+        "price" => 3.50
+    ),
+    array(
+        "name" => "Iced Latte",
+        "price" => 5.99
+    ),
+    array(
+        "name" => "Blueberry Muffin",
+        "price" => 2.99
+    )
+);
+
+echo json_encode($items);
+?>
