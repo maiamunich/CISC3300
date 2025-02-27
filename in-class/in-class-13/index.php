b3c4ea97d3376981af0616c0b88057cc09a53361
Maia Munich <maia.munich@gmail.com>
2025-02-27 16:26:46 -0500

    adding changes

---
+++ in-class/in-class-13/index.php
@@ -0,0 +1,33 @@
+<?php
+header('Content-Type: application/json');
+
+// Sample business data - you can modify this
+$items = array(
+    array(
+        "name" => "EXPLOSION",
+        "color" => "red"
+    ),
+    array(
+        "name" => "Baby",
+        "color" => "baby pink"
+    ),
+    array(
+        "name" => "Ocean Glass",
+        "color" => "sea green"
+    ),
+    array(
+        "name" => "Blueberry",
+        "color" => "blue-purple hue"
+    ),
+    array(
+        "name" => "Chocolate",
+        "color" => "dark brown"
+    ),
+    array(
+        "name" => "Vanilla Swirl",
+        "color" => "white & black"
+    )
+);
+
+echo json_encode($items);
+?>
