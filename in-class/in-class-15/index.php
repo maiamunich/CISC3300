e723140bd21ae298199f434ac7906c0a00009d83
Maia Munich <maia.munich@gmail.com>
2025-03-06 17:12:02 -0500

    sdftyui

--- in-class/in-class-15/index.php
+++ in-class/in-class-15/index.php
@@ -1,14 +1,9 @@
 <?php
-require_once __DIR__ . '/Controllers/PostsController.php';
 
-// Get the URL from the request
-$url = isset($_GET['url']) ? $_GET['url'] : '';
+require "../app/Router.php";
 
-// Basic routing
-if ($_SERVER['REQUEST_METHOD'] === 'GET' && $url === 'posts') {
-    header('Content-Type: application/json');
-    $controller = new PostsController();
-    echo $controller->index();
-} else {
-    echo "Welcome to the Posts API. Use /posts to get all posts.";
-} 
+use app\Router;
+
+
+$router = new Router();
+$router->handleRoutes();
