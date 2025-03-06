87b0ba0de0d1f7a125c6653c1acd94a96ac0ea19
Maia Munich <maia.munich@gmail.com>
2025-03-06 17:13:08 -0500

    PLEASE PLES PELS WORKKKKK

--- in-class/in-class-15/index.php
+++ in-class/in-class-15/index.php
@@ -1,9 +1,24 @@
 <?php
+require_once __DIR__ . '/Controllers/PostsController.php';
 
-require "../app/Router.php";
+// Debug output
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
-use app\Router;
+// Get the path from the URL
+$request_uri = $_SERVER['REQUEST_URI'];
+$path = parse_url($request_uri, PHP_URL_PATH);
+$path = trim($path, '/');
 
+// Debug output
+echo "Path: " . $path . "<br>";
 
-$router = new Router();
-$router->handleRoutes();
+// Basic routing
+if ($_SERVER['REQUEST_METHOD'] === 'GET' && $path === 'posts') {
+    header('Content-Type: application/json');
+    $controller = new PostsController();
+    $result = $controller->index();
+    echo $result;
+} else {
+    echo "Welcome to the Posts API. Use /posts to get all posts.";
+}
