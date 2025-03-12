69f347a68a637690dcae175046d5d993779953cc
Maia Munich <maia.munich@gmail.com>
2025-03-12 10:53:53 -0400

    sdknfskdn

--- assignment/homework-7/controllers/NoteController.php
+++ assignment/homework-7/controllers/NoteController.php
@@ -1,31 +1,44 @@
 <?php
 class NoteController {
     public function showForm() {
-        require 'views/note_form.php';
+        $html = file_get_contents('views/note_form.html');
+        $html = str_replace('[ERROR_MESSAGE]', '', $html);
+        $html = str_replace('[SUCCESS_MESSAGE]', '', $html);
+        $html = str_replace('[TITLE_VALUE]', '', $html);
+        $html = str_replace('[DESCRIPTION_VALUE]', '', $html);
+        echo $html;
     }
 
     public function createNote($post) {
         $title = trim($post['title'] ?? '');
         $description = trim($post['description'] ?? '');
         
+        $html = file_get_contents('views/note_form.html');
+        
         // Validation
         if (strlen($title) <= 3) {
-            $error = "Title must be more than 3 characters long";
+            $error = '<div class="error">Title must be more than 3 characters long</div>';
+            $html = str_replace('[ERROR_MESSAGE]', $error, $html);
+            $html = str_replace('[SUCCESS_MESSAGE]', '', $html);
         }
         elseif (strlen($description) <= 10) {
-            $error = "Description must be more than 10 characters long";
+            $error = '<div class="error">Description must be more than 10 characters long</div>';
+            $html = str_replace('[ERROR_MESSAGE]', $error, $html);
+            $html = str_replace('[SUCCESS_MESSAGE]', '', $html);
         }
-        
-        if (isset($error)) {
-            // If there's an error, show the form again
-            require 'views/note_form.php';
-        } else {
+        else {
             // Convert special characters
             $title = htmlspecialchars($title, ENT_QUOTES, 'UTF-8');
             $description = htmlspecialchars($description, ENT_QUOTES, 'UTF-8');
-            $success = "Note created successfully!";
-            require 'views/note_form.php';
+            $html = str_replace('[ERROR_MESSAGE]', '', $html);
+            $html = str_replace('[SUCCESS_MESSAGE]', '<div class="success">Note created successfully!</div>', $html);
         }
+        
+        // Replace form values
+        $html = str_replace('[TITLE_VALUE]', htmlspecialchars($title), $html);
+        $html = str_replace('[DESCRIPTION_VALUE]', htmlspecialchars($description), $html);
+        
+        echo $html;
     }
 }
 ?> 
